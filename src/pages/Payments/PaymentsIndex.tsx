f0ceae234c615ab56a6f4f9701c44a08595ceaea
EgorDubovik <posik.web.m@gmail.com>
2024-04-14 18:41:47 -0500

    google map link from customer

--- src/pages/Payments/PaymentsIndex.tsx
+++ src/pages/Payments/PaymentsIndex.tsx
@@ -4,8 +4,11 @@ import IconTrashLines from '../../components/Icon/IconTrashLines';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
 import { viewCurrency } from '../../helpers/helper';
+import { PageCirclePrimaryLoader } from '../../components/loading/PageLoading';
+import { PageLoadError } from '../../components/loading/Errors';
 
 const PaymentsIndex = () => {
+	const [loadingStatus, setLoadingStatus] = useState<string>('loading');
 	const [startTime, setStartTime] = useState();
 	const [endTime, setEndTime] = useState();
 	const [payments, setPayments] = useState([]);
@@ -16,19 +19,25 @@ const PaymentsIndex = () => {
 	}, [payments]);
 
 	useEffect(() => {
+		setLoadingStatus('loading');
 		axiosClient
 			.get('/payments')
 			.then((response) => {
 				console.log(response.data);
 				setPayments(response.data.payments);
+				setLoadingStatus('success');
 			})
 			.catch((error) => {
 				console.log(error);
+				setLoadingStatus('error');
 			});
 	}, []);
 
 	return (
 		<div>
+			{loadingStatus === 'loading' && <PageCirclePrimaryLoader />}
+			{loadingStatus === 'error' && <PageLoadError />}
+			{loadingStatus === 'success' && 
 			<div className="grid grid-cols-1 md:grid-cols-4 gap-4">
 				<div className="md:col-span-3 panel p-0 border-0 overflow-hidden">
 					<div className="table-responsive">
@@ -57,7 +66,7 @@ const PaymentsIndex = () => {
 													Appointment at {payment.appointment ? moment(payment.appointment.created_at).format('d-m-Y') : 'Unknow'}
 												</Link>
 											</td>
-											<td className="whitespace-nowrap">{viewCurrency(payment.amount)}</td>
+											<td className={"whitespace-nowrap"+(payment.amount>0) ? 'text-success' : 'text-danger'}>{viewCurrency(payment.amount)}</td>
 											<td>{moment(payment.date).format('DD MMM YYYY')}</td>
 											<td>{payment.payment_type}</td>
 											<td>
@@ -76,6 +85,7 @@ const PaymentsIndex = () => {
 					<h2>Technitial</h2>
 				</div>
 			</div>
+			}
 		</div>
 	);
 };
