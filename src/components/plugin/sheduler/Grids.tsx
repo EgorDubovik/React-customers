a761f7a8897286889b3bc71bcdd1a2b7203f7a57
EgorDubovik <posik.web.m@gmail.com>
2024-04-26 08:27:38 -0500

    fixes in payments graph

--- src/components/plugin/sheduler/Grids.tsx
+++ src/components/plugin/sheduler/Grids.tsx
@@ -1,135 +1,145 @@
 import { useRef } from 'react';
 import moment from 'moment';
 
-const Grids = (props:any) => {
-   const appointmentList = props.appointmentList || [];
-   const daysArray = props.daysArray || [];
-   const startTime = props.startTime || null;
-   const endTime = props.endTime || null;
-   const timesArray = props.timesArray || [];
-   const totalDuration = props.totalDuration || null;
-   const blockHeight = props.blockHeight || 50;
+const Grids = (props: any) => {
+	const appointmentList = props.appointmentList || [];
+	const daysArray = props.daysArray || [];
+	const startTime = props.startTime || null;
+	const endTime = props.endTime || null;
+	const timesArray = props.timesArray || [];
+	const totalDuration = props.totalDuration || null;
+	const blockHeight = props.blockHeight || 50;
 
-   const onAppointmentClick = props.onAppointmentClick || null;
-   const setAppointmentForCurentDate = props.setAppointmentForCurentDate || null;
+	const onAppointmentClick = props.onAppointmentClick || null;
+	const setAppointmentForCurentDate = props.setAppointmentForCurentDate || null;
 
+	const dayInnerRef = useRef(null);
+	const dayInnerHeight = useRef(0);
+	const dayInnerOffsettop = useRef(0);
 
-   const dayInnerRef = useRef(null);
-   const dayInnerHeight = useRef(0);
-   const dayInnerOffsettop = useRef(0);
-   
-   const calculateTimePercentage = (time:any) => {
-      const relativeTime = time.clone().set({hour: startTime.hour(), minute: startTime.minute()});
-      const elaspseTime = moment.duration(time.diff(relativeTime));
-      return (elaspseTime.asMinutes() / totalDuration.asMinutes()) * 100;
-   }
-   const procentToMinutes = (procent:number) => {
-      return Math.round((totalDuration.asMinutes() * procent) / 100);
-   }
+	const calculateTimePercentage = (time: any) => {
+		const relativeTime = time.clone().set({ hour: startTime.hour(), minute: startTime.minute() });
+		const elaspseTime = moment.duration(time.diff(relativeTime));
+		return (elaspseTime.asMinutes() / totalDuration.asMinutes()) * 100;
+	};
+	const procentToMinutes = (procent: number) => {
+		return Math.round((totalDuration.asMinutes() * procent) / 100);
+	};
 
-   const isDragging = useRef(false);
-   const startPosition = useRef(0);
-   const appointmentOffsetTop = useRef(0);
-   const appointmentOffsetInner = useRef(0);
-   const appointmentRef = useRef<HTMLDivElement | null>(null);
-   const appointmentProcentHeightRef = useRef(0);
-   const appointmentPxHeightRef = useRef(0);
-   const appointmentIndex = useRef(0);
-   const newProcentTop = useRef(0);
-   const deltaProcent = (15 * 100) / totalDuration.asMinutes();
-   const deltaPx = useRef(0);
-   const isMoving = useRef(false);
+	const isDragging = useRef(false);
+	const startPosition = useRef(0);
+	const appointmentOffsetTop = useRef(0);
+	const appointmentOffsetInner = useRef(0);
+	const appointmentRef = useRef<HTMLDivElement | null>(null);
+	const appointmentProcentHeightRef = useRef(0);
+	const appointmentPxHeightRef = useRef(0);
+	const appointmentIndex = useRef(0);
+	const newProcentTop = useRef(0);
+	const deltaProcent = (15 * 100) / totalDuration.asMinutes();
+	const deltaPx = useRef(0);
+	const isMoving = useRef(false);
 
+	const handleMouseDown = (e: any, index: number) => {
+		if (e.target.className.indexOf('appointment-title') != -1) return;
+		appointmentIndex.current = index;
+		appointmentRef.current = e.currentTarget;
 
-   const handleMouseDown = (e:any,index:number) => {
-      if(e.target.className.indexOf('appointment-title') != -1) return;
-      appointmentIndex.current = index;
-      appointmentRef.current = e.currentTarget;
+		const dayInnerElement = e.currentTarget.closest('.day-inner');
 
+		dayInnerHeight.current = dayInnerElement.getBoundingClientRect().height;
+		dayInnerOffsettop.current = dayInnerElement.getBoundingClientRect().top;
+		if (appointmentRef.current) {
+			appointmentOffsetTop.current = appointmentRef.current.getBoundingClientRect().top;
+			appointmentPxHeightRef.current = appointmentRef.current.getBoundingClientRect().height;
+			appointmentRef.current.style.zIndex = '100';
+			appointmentRef.current.style.width = '100%';
+			appointmentRef.current.style.left = '0%';
+		}
+		appointmentOffsetInner.current = appointmentOffsetTop.current - dayInnerOffsettop.current;
+		appointmentProcentHeightRef.current = calculateTimePercentage(appointmentList[index].end) - calculateTimePercentage(appointmentList[index].start);
+		startPosition.current = e.clientY;
+		deltaPx.current = (deltaProcent * dayInnerHeight.current) / 100;
+		isDragging.current = true;
+	};
 
-      const dayInnerElement = e.currentTarget.closest('.day-inner');
+	const handleMouseMove = (e: any) => {
+		if (!isDragging.current) return;
 
-      dayInnerHeight.current = dayInnerElement.getBoundingClientRect().height;
-      dayInnerOffsettop.current = dayInnerElement.getBoundingClientRect().top;
-      if(appointmentRef.current){
-         appointmentOffsetTop.current = appointmentRef.current.getBoundingClientRect().top;
-         appointmentPxHeightRef.current = appointmentRef.current.getBoundingClientRect().height;
-         appointmentRef.current.style.zIndex = '100';
-         appointmentRef.current.style.width = '100%';
-         appointmentRef.current.style.left = '0%';
-      }
-      appointmentOffsetInner.current = appointmentOffsetTop.current - dayInnerOffsettop.current;
-      appointmentProcentHeightRef.current = calculateTimePercentage(appointmentList[index].end) - calculateTimePercentage(appointmentList[index].start);
-      startPosition.current = e.clientY;
-      deltaPx.current = (deltaProcent * dayInnerHeight.current) / 100;
-      isDragging.current = true;
-   }
-   
-   const handleMouseMove = (e:any) => {
-      if(!isDragging.current) return;
+		let changedPosition = e.clientY - startPosition.current;
+		let newTopPosition = appointmentOffsetInner.current + changedPosition; // new position of appointment in px from top of dayInner
+		if (newTopPosition >= 0 && appointmentPxHeightRef.current + newTopPosition <= dayInnerHeight.current) {
+			isMoving.current = true;
+			let t = Math.round(newTopPosition / deltaPx.current);
+			newTopPosition = t * deltaPx.current;
+			newProcentTop.current = (newTopPosition / dayInnerHeight.current) * 100;
+			if (appointmentRef.current) appointmentRef.current.style.top = newProcentTop.current + '%';
+		}
+	};
 
-      let changedPosition = e.clientY - startPosition.current;
-      let newTopPosition = appointmentOffsetInner.current+changedPosition; // new position of appointment in px from top of dayInner
-      if(newTopPosition >= 0 && appointmentPxHeightRef.current+newTopPosition <= dayInnerHeight.current){
-         isMoving.current = true;
-         let t = Math.round(newTopPosition / deltaPx.current);
-         newTopPosition = t * deltaPx.current;
-         newProcentTop.current = (newTopPosition / dayInnerHeight.current) * 100;
-         if(appointmentRef.current)
-            appointmentRef.current.style.top = newProcentTop.current + '%';
-      }
-   }
+	const handleMouseUp = () => {
+		if (!isDragging.current || !isMoving.current) {
+			isDragging.current = false;
+			isMoving.current = false;
+			return;
+		}
+		appointmentList[appointmentIndex.current].start = moment(appointmentList[appointmentIndex.current].start.format('YYYY-MM-DD') + ' ' + startTime.format('HH:mm')).add(
+			procentToMinutes(newProcentTop.current),
+			'minutes'
+		);
+		appointmentList[appointmentIndex.current].end = moment(appointmentList[appointmentIndex.current].end.format('YYYY-MM-DD') + ' ' + startTime.format('HH:mm')).add(
+			procentToMinutes(newProcentTop.current + appointmentProcentHeightRef.current),
+			'minutes'
+		);
+		isDragging.current = false;
+		if (appointmentRef.current) appointmentRef.current.style.zIndex = '1';
+		isMoving.current = false;
+		setAppointmentForCurentDate(appointmentList);
+	};
 
-   const handleMouseUp = () => {
-      if(!isDragging.current || !isMoving.current){
-         isDragging.current = false;
-         isMoving.current = false;
-         return;
-      }
-      appointmentList[appointmentIndex.current].start = moment(appointmentList[appointmentIndex.current].start.format('YYYY-MM-DD')+' '+startTime.format('HH:mm')).add(procentToMinutes(newProcentTop.current), 'minutes');
-      appointmentList[appointmentIndex.current].end = moment(appointmentList[appointmentIndex.current].end.format('YYYY-MM-DD')+' '+startTime.format('HH:mm')).add(procentToMinutes(newProcentTop.current+appointmentProcentHeightRef.current), 'minutes');
-      isDragging.current = false;
-      if(appointmentRef.current)
-         appointmentRef.current.style.zIndex = '1';
-      isMoving.current = false;
-      setAppointmentForCurentDate(appointmentList);
-   }
-
-   return (
-      <div className={"dates grid  w-full"} style={{ gridTemplateColumns: `repeat(${daysArray.length}, minmax(0, 1fr))` }}>
-      {daysArray.map((day:any, index:number) => (
-         <div className="date pt-2 first:border-0 border-l dark:border-gray-600 border-gray-300" key={index}>
-            <div className='day-inner relative h-full' ref={dayInnerRef}>
-               <div className="appointments-list absolute h-full left-0 top-0 right-0" onMouseUp={handleMouseUp} onMouseMove={handleMouseMove}>
-                  
-                  { appointmentList.map((appointment:any, aindex:number) => 
-                     {
-                        
-                        if(appointment.start.format('ddd DD') === day) {
-
-                           return (
-                              <div key={appointment.title+"-"+aindex+appointment.top} onMouseDown={(e)=>{handleMouseDown(e,aindex)}}  className={"appointment text-[.8em]  absolute p-[2px] cursor-pointer"} style={{ height: appointment.height+'%', width: appointment.width+'%', left: appointment.left+'%', top: appointment.top+'%' }}>
-                                 <div className={"appointment-conteiner rounded absolute"} style={{ background:appointment.bg,inset:'1px',opacity:appointment.opacity ?? 0.8 }}></div>
-                                 <div className={(appointment.addClass ?? "")+' sticky font-bold'}>
-                                    <div className="appointment-title px-2 pt-1 hover:underline " onClick={()=>{onAppointmentClick(appointment)}}>{appointment.title}</div>
-                                    <div className="appointment-time px-2">{appointment.start.format('hh:mm A')} - {appointment.end.format('hh:mm A')}</div>
-                                 </div>
-                              </div>
-                           )
-                        }
-                     }
-                  )}
-                  
-               </div>
-               { timesArray.map((time:string, index:number) => (
-                  <div key={index} className={"date-time dark:border-gray-600 border-gray-300 border-t"} style={{ height:blockHeight+'px' }}></div>
-               ))}
-            </div>
-         </div>
-      ))}
-
-   </div>
-   )
-}
+	return (
+		<div className={'dates grid  w-full'} style={{ gridTemplateColumns: `repeat(${daysArray.length}, minmax(0, 1fr))` }}>
+			{daysArray.map((day: any, index: number) => (
+				<div className="date pt-2 first:border-0 border-l dark:border-gray-600 border-gray-300" key={index}>
+					<div className="day-inner relative h-full" ref={dayInnerRef}>
+						<div className="appointments-list absolute h-full left-0 top-0 right-0" onMouseUp={handleMouseUp} onMouseMove={handleMouseMove}>
+							{appointmentList.map((appointment: any, aindex: number) => {
+								if (appointment.start.format('ddd DD') === day) {
+									return (
+										<div
+											key={appointment.title + '-' + aindex + appointment.top}
+											onMouseDown={(e) => {
+												handleMouseDown(e, aindex);
+											}}
+											className={'appointment text-[.8em]  absolute p-[2px] cursor-pointer'}
+											style={{ height: appointment.height + '%', width: appointment.width + '%', left: appointment.left + '%', top: appointment.top + '%' }}
+										>
+											<div className={'appointment-conteiner rounded absolute'} style={{ background: appointment.bg, inset: '1px', opacity: appointment.opacity ?? 0.8 }}></div>
+											<div className={(appointment.addClass ?? '') + ' sticky font-bold'}>
+												<div
+													className="appointment-title px-2 pt-1 hover:underline "
+													onClick={() => {
+														onAppointmentClick(appointment);
+													}}
+												>
+													{appointment.title}
+												</div>
+												<div className="appointment-time px-2">
+													{appointment.start.format('hh:mm A')} - {appointment.end.format('hh:mm A')}
+												</div>
+											</div>
+										</div>
+									);
+								}
+							})}
+						</div>
+						{timesArray.map((time: string, index: number) => (
+							<div key={index} className={'date-time dark:border-gray-600 border-gray-300 border-t'} style={{ height: blockHeight + 'px' }}></div>
+						))}
+					</div>
+				</div>
+			))}
+		</div>
+	);
+};
 
 export default Grids;
--- src/pages/Payments/PaymentsIndex.tsx
+++ src/pages/Payments/PaymentsIndex.tsx
@@ -69,141 +69,157 @@ const PaymentsIndex = () => {
 		}
 	};
 
-	const options = {
-		chart: {
-			 height: 325,
-			 type: 'area',
-			 fontFamily: 'Nunito, sans-serif',
-			 zoom: {
-				  enabled: false,
-			 },
-			 toolbar: {
-				  show: false,
-			 },
-		},
+	const isRtl = false;
+	const isDark = true;
+	const revenueChart: any = {
+		series: [
+			{
+				name: 'Income',
+				data: [16800, 16800, 15500, 17800, 15500, 17000, 19000, 16000, 15000, 17000, 14000, 17000],
+			},
+			{
+				name: 'Expenses',
+				data: [16500, 17500, 16200, 17300, 16000, 19500, 16000, 17000, 16000, 19000, 18000, 19000],
+			},
+		],
+		options: {
+			chart: {
+				height: 325,
+				type: 'area',
+				fontFamily: 'Nunito, sans-serif',
+				zoom: {
+					enabled: false,
+				},
+				toolbar: {
+					show: false,
+				},
+			},
 
-		dataLabels: {
-			 enabled: false,
-		},
-		stroke: {
-			 show: true,
-			 curve: 'smooth',
-			 width: 2,
-			 lineCap: 'square',
-		},
-		dropShadow: {
-			 enabled: true,
-			 opacity: 0.2,
-			 blur: 10,
-			 left: -7,
-			 top: 22,
-		},
-		colors: ['#2196F3'],
-		markers: {
-			 discrete: [
-				  {
+			dataLabels: {
+				enabled: false,
+			},
+			stroke: {
+				show: true,
+				curve: 'smooth',
+				width: 2,
+				lineCap: 'square',
+			},
+			dropShadow: {
+				enabled: true,
+				opacity: 0.2,
+				blur: 10,
+				left: -7,
+				top: 22,
+			},
+			colors: isDark ? ['#2196F3', '#E7515A'] : ['#1B55E2', '#E7515A'],
+			markers: {
+				discrete: [
+					{
 						seriesIndex: 0,
 						dataPointIndex: 6,
 						fillColor: '#1B55E2',
 						strokeColor: 'transparent',
 						size: 7,
-				  },
-			 ],
-		},
-		labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-		xaxis: {
-			 axisBorder: {
-				  show: false,
-			 },
-			 axisTicks: {
-				  show: false,
-			 },
-			 crosshairs: {
-				  show: true,
-			 },
-			 labels: {
-				  offsetX: 0,
-				  offsetY: 5,
-				  style: {
+					},
+					{
+						seriesIndex: 1,
+						dataPointIndex: 5,
+						fillColor: '#E7515A',
+						strokeColor: 'transparent',
+						size: 7,
+					},
+				],
+			},
+			labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+			xaxis: {
+				axisBorder: {
+					show: false,
+				},
+				axisTicks: {
+					show: false,
+				},
+				crosshairs: {
+					show: true,
+				},
+				labels: {
+					offsetX: isRtl ? 2 : 0,
+					offsetY: 5,
+					style: {
 						fontSize: '12px',
 						cssClass: 'apexcharts-xaxis-title',
-				  },
-			 },
-		},
-		yaxis: {
-			 tickAmount: 7,
-			 labels: {
-				  formatter: (value: number) => {
-						return value / 1000 + 'K';
-				  },
-				  offsetX:  -10,
-				  offsetY: 0,
-				  style: {
+					},
+				},
+			},
+			yaxis: {
+				tickAmount: 7,
+				labels: {
+					formatter: (value: number) => {
+						if(value > 1000) return '$' + value / 1000 + 'k';
+						return viewCurrency(value);
+					},
+					offsetX: -10,
+					offsetY: 0,
+					style: {
 						fontSize: '12px',
 						cssClass: 'apexcharts-yaxis-title',
-				  },
-			 },
-			 opposite: false,
-		},
-		grid: {
-			 borderColor: '#191E3A',
-			 strokeDashArray: 5,
-			 xaxis: {
-				  lines: {
+					},
+				},
+				opposite: isRtl ? true : false,
+			},
+			grid: {
+				borderColor: isDark ? '#191E3A' : '#E0E6ED',
+				strokeDashArray: 5,
+				xaxis: {
+					lines: {
 						show: true,
-				  },
-			 },
-			 yaxis: {
-				  lines: {
+					},
+				},
+				yaxis: {
+					lines: {
 						show: false,
-				  },
-			 },
-			 padding: {
-				  top: 0,
-				  right: 0,
-				  bottom: 0,
-				  left: 0,
-			 },
-		},
-		legend: {
-			 position: 'top',
-			 horizontalAlign: 'right',
-			 fontSize: '16px',
-			 markers: {
-				  width: 10,
-				  height: 10,
-				  offsetX: -2,
-			 },
-			 itemMargin: {
-				  horizontal: 10,
-				  vertical: 5,
-			 },
-		},
-		tooltip: {
-			 marker: {
-				  show: true,
-			 },
-			 x: {
-				  show: false,
-			 },
-		},
-		fill: {
-			 type: 'gradient',
-			 gradient: {
-				  shadeIntensity: 1,
-				  inverseColors: !1,
-				  opacityFrom: 0.19,
-				  opacityTo: 0.05,
-				  stops: [100, 100],
-			 },
+					},
+				},
+				padding: {
+					top: 0,
+					right: 0,
+					bottom: 0,
+					left: 0,
+				},
+			},
+			legend: {
+				position: 'top',
+				horizontalAlign: 'right',
+				fontSize: '16px',
+				markers: {
+					width: 10,
+					height: 10,
+					offsetX: -2,
+				},
+				itemMargin: {
+					horizontal: 10,
+					vertical: 5,
+				},
+			},
+			tooltip: {
+				marker: {
+					show: true,
+				},
+				x: {
+					show: false,
+				},
+			},
+			fill: {
+				type: 'gradient',
+				gradient: {
+					shadeIntensity: 1,
+					inverseColors: !1,
+					opacityFrom: isDark ? 0.19 : 0.28,
+					opacityTo: 0.05,
+					stops: isDark ? [100, 100] : [45, 100],
+				},
+			},
 		},
 	};
-	const series = [
-		{
-			name: 'Total',
-			data: payments.map((payment: any) => payment.amount),
-		},
-	];
 
 	return (
 		<div>
@@ -240,13 +256,7 @@ const PaymentsIndex = () => {
 					<div className="panel p-4">
 						<h2>Graph</h2>
 						<div>
-							<ReactApexChart
-								options={options}
-								series={series}
-								type="area"
-								height={350}
-								width={'100%'}
-								className="shadow-lg"/>
+							<ReactApexChart series={revenueChart.series} options={revenueChart.options} type="area" height={325} />
 						</div>
 					</div>
 					<div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-6 text-center">
