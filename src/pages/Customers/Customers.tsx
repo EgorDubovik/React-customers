f3f9e15ae4dd1d854442e66b2bfe6bd1ea8325f2
EgorDubovik <posik.web.m@gmail.com>
2024-05-10 08:31:26 -0500

    view empty message if no customers found

--- src/pages/Customers/Customers.tsx
+++ src/pages/Customers/Customers.tsx
@@ -156,128 +156,141 @@ const Contacts = () => {
 			)}
 			{loadingStatus === 'success' && (
 				<>
-					{viewType === 'list' && (
-						<div className="mt-5 panel p-0 border-0 overflow-hidden">
-							<div className="datatables pagination-padding">
-								<DataTable
-									className="whitespace-nowrap table-hover invoice-table pb-4"
-									records={records}
-									columns={[
-										{
-											accessor: 'ID',
-											sortable: true,
-											render: ({ id }) => <span>{id}</span>,
-										},
-										{
-											accessor: 'Customer Name',
-											sortable: true,
-											render: ({ name, id }) => (
-												<div className="flex items-center font-semibold">
-													<Link to={`/customer/${id}`} className="text-primary underline hover:no-underline">
-														{name}
-													</Link>
-												</div>
-											),
-										},
-										{
-											accessor: 'phone',
-											sortable: true,
-											render: ({ phone }) => <div className="font-semibold">{phone}</div>,
-										},
-										{
-											accessor: 'address',
-											sortable: false,
-											render: ({ address }) => <div className="font-semibold">{address[0].full}</div>,
-										},
-										{
-											accessor: 'email',
-											sortable: true,
-											render: ({ email }) => <div className="font-semibold">{email}</div>,
-										},
-										{
-											accessor: 'date',
-											sortable: true,
-											render: ({ created_at }) => <div className="font-semibold">{moment(created_at).format('MMMM Do YYYY, h:mm:ss a')}</div>,
-										},
-
-										{
-											accessor: 'action',
-											title: 'Actions',
-											sortable: false,
-											textAlignment: 'center',
-											render: ({ id }) => (
-												<div className="flex gap-4 items-center w-max mx-auto">
-													<button type="button" className="btn btn-sm btn-outline-warning" onClick={() => editUser({ id })}>
-														Edit
-													</button>
-													<button type="button" className="btn btn-sm btn-outline-info" onClick={() => navigator('/customer/' + id)}>
-														View
-													</button>
-												</div>
-											),
-										},
-									]}
-									highlightOnHover
-									totalRecords={totalRecords}
-									recordsPerPage={pageSize}
-									page={page}
-									onPageChange={(p) => setPage(p)}
-									recordsPerPageOptions={PAGE_SIZES}
-									onRecordsPerPageChange={setPageSize}
-									sortStatus={sortStatus}
-									onSortStatusChange={setSortStatus}
-									paginationText={({ from, to, totalRecords }) => `Showing  ${from} to ${to} of ${totalRecords} entries`}
-								/>
+					{records.length === 0 ? (
+						<div className="mt-4">
+							<div className="flex items-center justify-center">
+								<div className="text-center">
+									<h2 className="text-xl">No Customers Found</h2>
+									<Link to={"/customers/create"} className="text-primary">Create new customer or search again</Link>
+								</div>
 							</div>
 						</div>
-					)}
-
-					{viewType === 'grid' && (
+					) : (
 						<>
-							<div className="grid 2xl:grid-cols-4 xl:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6 mt-5 w-full">
-								{records.map((customer: any) => {
-									return (
-										<div className="bg-white dark:bg-[#1a294166] rounded-md shadow" key={customer.id}>
-											<div className="p-3">
-												<div className="flex items-center justify-between relative">
-													<div className="absolute top-0 right-0 z-10">
-														<div className="dropdown">
-															<Dropdown offset={[0, 5]} btnClassName="align-middle" button={<IconHorizontalDots className="rotate-90 opacity-70" />}>
-																<ul className="whitespace-nowrap">
-																	<li>
-																		<button type="button" onClick={() => editUser(customer)}>
-																			<IconPencilPaper className="w-4.5 h-4.5 ltr:mr-2 rtl:ml-2 shrink-0" />
-																			Edit
-																		</button>
-																	</li>
-																	<li>
-																		<button type="button" onClick={() => navigator('/customer/' + customer.id)}>
-																			<IconEye className="w-4.5 h-4.5 mr-2 shrink-0" />
-																			View
-																		</button>
-																	</li>
-																</ul>
-															</Dropdown>
+							{viewType === 'list' && (
+								<div className="mt-5 panel p-0 border-0 overflow-hidden">
+									<div className="datatables pagination-padding">
+										<DataTable
+											className="whitespace-nowrap table-hover invoice-table pb-4"
+											records={records}
+											columns={[
+												{
+													accessor: 'ID',
+													sortable: true,
+													render: ({ id }) => <span>{id}</span>,
+												},
+												{
+													accessor: 'Customer Name',
+													sortable: true,
+													render: ({ name, id }) => (
+														<div className="flex items-center font-semibold">
+															<Link to={`/customer/${id}`} className="text-primary underline hover:no-underline">
+																{name}
+															</Link>
+														</div>
+													),
+												},
+												{
+													accessor: 'phone',
+													sortable: true,
+													render: ({ phone }) => <div className="font-semibold">{phone}</div>,
+												},
+												{
+													accessor: 'address',
+													sortable: false,
+													render: ({ address }) => <div className="font-semibold">{address[0].full}</div>,
+												},
+												{
+													accessor: 'email',
+													sortable: true,
+													render: ({ email }) => <div className="font-semibold">{email}</div>,
+												},
+												{
+													accessor: 'date',
+													sortable: true,
+													render: ({ created_at }) => <div className="font-semibold">{moment(created_at).format('MMMM Do YYYY, h:mm:ss a')}</div>,
+												},
+
+												{
+													accessor: 'action',
+													title: 'Actions',
+													sortable: false,
+													textAlignment: 'center',
+													render: ({ id }) => (
+														<div className="flex gap-4 items-center w-max mx-auto">
+															<button type="button" className="btn btn-sm btn-outline-warning" onClick={() => editUser({ id })}>
+																Edit
+															</button>
+															<button type="button" className="btn btn-sm btn-outline-info" onClick={() => navigator('/customer/' + id)}>
+																View
+															</button>
+														</div>
+													),
+												},
+											]}
+											highlightOnHover
+											totalRecords={totalRecords}
+											recordsPerPage={pageSize}
+											page={page}
+											onPageChange={(p) => setPage(p)}
+											recordsPerPageOptions={PAGE_SIZES}
+											onRecordsPerPageChange={setPageSize}
+											sortStatus={sortStatus}
+											onSortStatusChange={setSortStatus}
+											paginationText={({ from, to, totalRecords }) => `Showing  ${from} to ${to} of ${totalRecords} entries`}
+										/>
+									</div>
+								</div>
+							)}
+
+							{viewType === 'grid' && (
+								<>
+									<div className="grid 2xl:grid-cols-4 xl:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6 mt-5 w-full">
+										{records.map((customer: any) => {
+											return (
+												<div className="bg-white dark:bg-[#1a294166] rounded-md shadow" key={customer.id}>
+													<div className="p-3">
+														<div className="flex items-center justify-between relative">
+															<div className="absolute top-0 right-0 z-10">
+																<div className="dropdown">
+																	<Dropdown offset={[0, 5]} btnClassName="align-middle" button={<IconHorizontalDots className="rotate-90 opacity-70" />}>
+																		<ul className="whitespace-nowrap">
+																			<li>
+																				<button type="button" onClick={() => editUser(customer)}>
+																					<IconPencilPaper className="w-4.5 h-4.5 ltr:mr-2 rtl:ml-2 shrink-0" />
+																					Edit
+																				</button>
+																			</li>
+																			<li>
+																				<button type="button" onClick={() => navigator('/customer/' + customer.id)}>
+																					<IconEye className="w-4.5 h-4.5 mr-2 shrink-0" />
+																					View
+																				</button>
+																			</li>
+																		</ul>
+																	</Dropdown>
+																</div>
+															</div>
+															<div className="user-info w-full">
+																<Link to={`/customer/${customer.id}`} className="text-primary">
+																	<div className="user-name w-3/4 font-bold dark:text-white">{customer.name}</div>
+																</Link>
+																<div className="">{customer.phone}</div>
+															</div>
+															{/* <div className='text-center px-4 text-[12px]'>paid $126.56</div> */}
+														</div>
+														<div className="user-address mt-3 ml-1 flex overflow-hidden">
+															<IconMapPin />
+															<div className="ml-2">{customer.address[0].full}</div>
 														</div>
 													</div>
-													<div className="user-info w-full">
-														<Link to={`/customer/${customer.id}`} className="text-primary">
-															<div className="user-name w-3/4 font-bold dark:text-white">{customer.name}</div>
-														</Link>
-														<div className="">{customer.phone}</div>
-													</div>
-													{/* <div className='text-center px-4 text-[12px]'>paid $126.56</div> */}
 												</div>
-												<div className="user-address mt-3 ml-1 flex overflow-hidden">
-													<IconMapPin />
-													<div className="ml-2">{customer.address[0].full}</div>
-												</div>
-											</div>
-										</div>
-									);
-								})}
-							</div>
-							<Pagination />
+											);
+										})}
+									</div>
+									<Pagination />
+								</>
+							)}
 						</>
 					)}
 				</>
