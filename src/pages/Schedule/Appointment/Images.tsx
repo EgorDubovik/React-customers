69523590a8892e6e3583efc0cdf75b324e88d853
EgorDubovik <posik.web.m@gmail.com>
2024-06-21 09:59:04 -0500

    view images as gallery

--- src/pages/Schedule/Appointment/Images.tsx
+++ src/pages/Schedule/Appointment/Images.tsx
@@ -4,7 +4,6 @@ import axios from 'axios';
 import IconPlus from '../../../components/Icon/IconPlus';
 import axiosClient from '../../../store/axiosClient';
 import { useAppointmentContext } from '../../../context/AppointmentContext';
-
 const Images = (props: any) => {
 	const appointmentId = props.appointmentId || 0;
 	const { appointment, updateImages } = useAppointmentContext();
@@ -13,7 +12,7 @@ const Images = (props: any) => {
 	const fileInputRef = useRef(null);
 	const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
 	const [showGallery, setShowGallery] = useState(false);
-	const [showImage, setShowImage] = useState<string>('');
+	const [showImageIndex, setShowImageIndex] = useState(0);
 
 	useEffect(() => {
 		handleUpload();
@@ -60,12 +59,27 @@ const Images = (props: any) => {
 		}
 	};
 
-	const openGallery = (path:string) => {
+	const openGallery = (index:number) => {
 		console.log('open gallery');
-		setShowImage(path);
+		setShowImageIndex(index);
 		setShowGallery(true);
-
 	};
+	const setNextImage = () => {
+		if(showImageIndex < images.length - 1){
+			setShowImageIndex(showImageIndex + 1);
+		} else {
+			setShowImageIndex(0);
+		}
+	}
+
+	const setPrevImage = () => {
+		if(showImageIndex > 0){
+			setShowImageIndex(showImageIndex - 1);
+		} else {
+			setShowImageIndex(images.length - 1);
+		}
+	}
+
 
 	return (
 		<>
@@ -80,16 +94,30 @@ const Images = (props: any) => {
 			<div className="grid grid-cols-3 md:grid-cols-3 lg:grid-cols-6 gap-3 p-4">
 				{images.map((image: any, index: number) => (
 					<div key={index} className="relative">
-						<img src={image.path} className="w-full h-20 object-cover rounded-lg cursor-pointer" onClick={()=>openGallery(image.path)} />
+						<img src={image.path} className="w-full h-20 object-cover rounded-lg cursor-pointer" onClick={() => openGallery(index)} />
 					</div>
 				))}
 			</div>
 			{showGallery && (
 				<>
-					<div className="z-30 fixed bottom-0 top-0 left-0 right-0 bg-black bg-opacity-80" ></div>
-					<div className="z-30 fixed bottom-0 top-0 left-0 right-0" onClick={() => setShowGallery(false)}>
-						<div className="absolute z-50 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-							<img src={showImage} className="w-full h-full object-cover rounded-lg" />
+					<div className="z-60 fixed bottom-0 top-0 left-0 right-0 bg-black bg-opacity-80 "></div>
+					<div className="z-80 fixed top-0 left-0 w-10 md:w-20 h-full cursor-pointer bg-black bg-opacity-80 hover:bg-opacity-100" onClick={setPrevImage}>
+						<div className="flex items-center justify-center h-full">
+							<svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+								<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-9-7 9-7" />
+							</svg>
+						</div>
+					</div>
+					<div className="z-70 fixed bottom-0 top-0 left-0 right-0" onClick={() => setShowGallery(false)}>
+						<div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+							<img src={images[showImageIndex]?.path} className="w-full h-full object-cover rounded-lg" />
+						</div>
+					</div>
+					<div className="z-80 fixed right-0 top-0 w-10 md:w-20 h-full cursor-pointer bg-black bg-opacity-80 hover:bg-opacity-100" onClick={setNextImage}>
+						<div className="flex items-center justify-center h-full">
+							<svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+								<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+							</svg>
 						</div>
 					</div>
 				</>
